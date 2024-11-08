cca790da3fb8e6e10735da5f02faedc00ac09aa0
Goydkombat <bep466@gmail.com>
2024-11-08 17:00:02 +0500

    machine.ts

---
+++ machine.ts
@@ -0,0 +1,9 @@
+import { createMachine } from "xstate";
+
+createMachine({})
+createMachine({
+/** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgAoBbAQwGMALASwzAEp8QAHLWKgFyqw0YA9EAjACZ0AT0FDkU5EA */})
+createMachine({
+    /** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgAoBbAQwGMALASwzAEp8QAHLWKgFyqw0YA9EAjACZ0AT0FDkU5EA */
+    id: 'test'
+})
